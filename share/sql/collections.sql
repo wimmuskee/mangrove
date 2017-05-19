bd53e7c8bf228ed62b87890e2cc8f76c4580f98a
Wim Muskee <wimmuskee@gmail.com>
2017-05-19 12:54:41 +0200

    add pushed column to collections, introduce db migrations

--- share/sql/collections.sql
+++ share/sql/collections.sql
@@ -3,6 +3,8 @@ drop table if exists collections;
 create table collections (
 id int(8) not null primary key auto_increment,
 configuration varchar(40) not null,
-updated int(11) not null default 0 );
+updated int(11) not null default 0,
+pushed int(11) not null default 0 );
+
 
 alter table collections add unique unique_configuration (configuration);
---
+++ share/sql/migrations/20170519.sql
@@ -0,0 +1,1 @@
+alter table collections add column pushed int(11) not null default 0 after updated;
