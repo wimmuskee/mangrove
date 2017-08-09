be4c9ab56852865ef81a43af3d202392ac813e31
Wim Muskee <wimmuskee@gmail.com>
2017-08-09 14:48:43 +0200

    retrieve saved identifier from retrieve row, not input

--- mangrove_libs/interface.py
+++ mangrove_libs/interface.py
@@ -38,7 +38,7 @@ class Interface:
 
 		if row:
 			self.DB.updateRecord(lom,oaidc,record["original_id"])
-			identifier = record["identifier"]
+			identifier = row["identifier"]
 		else:
 			identifier = self.getNewIdentifier()
 			self.DB.insertRecord(identifier,lom,oaidc,setspec,record["original_id"])
