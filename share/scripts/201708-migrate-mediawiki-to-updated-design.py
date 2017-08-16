13ec1200c1fedcea90cd443048be266037c33098
Wim Muskee <wimmuskee@gmail.com>
2017-08-16 11:32:13 +0200

    use separate db configs for mediawiki migration

--- share/scripts/201708-migrate-mediawiki-to-updated-design.py
+++ share/scripts/201708-migrate-mediawiki-to-updated-design.py
@@ -2,6 +2,7 @@
 # Mediawiki records are the last to be updated to the current Mangrove design.
 # Most prominent is making the xml records at local harvest time.
 # For the migration, we have to do this here, based on the old database.
+# Keep the old database in the source config, and generic mangrove db in the common config.
 
 # copy script to application root and execute:
 # python2 201708-migrate-mediawiki-to-updated-design.py -s wikikids -d wikilom -u "http://wikikids.nl/"
@@ -18,7 +19,6 @@ import datetime
 
 parser = argparse.ArgumentParser(description='migration script for mediawiki records')
 parser.add_argument('-s', '--source', nargs=1, help='Mediawiki Data provider', metavar='datasource', dest='source')
-parser.add_argument('-d', '--old-db', nargs=1, help='old database name', metavar='database name', dest='olddb')
 parser.add_argument('-u', '--host', nargs=1, help='source location host', metavar='host', dest='host')
 
 args = parser.parse_args()
@@ -27,11 +27,6 @@ if args.source:
 	source = args.source[0]
 else:
 	parser.error('Input a valid source')
-
-if args.olddb:
-	olddb = args.olddb[0]
-else:
-	parser.error('Provide the database name where the old mediawiki data lives.')
 
 if args.host:
 	host = args.host[0]
@@ -42,11 +37,12 @@ else:
 configfile = "mangrove-crawler.cfg"
 config = common.getConfig(configfile,"common")
 config["configuration"] = source
+oldconfig = common.getConfig(configfile,source)
 
 # set classes
 DB = Database(config["db_host"],config["db_user"],config["db_passwd"],config["db_name"])
 DB.setCollectionInfo(source)
-OLDDB = Database(config["db_host"],config["db_user"],config["db_passwd"],olddb)
+OLDDB = Database(oldconfig["db_host"],oldconfig["db_user"],oldconfig["db_passwd"],oldconfig["db_name"])
 t = TextProcessor("","nl_NL")
 FS = Filesystem(config)
 
@@ -80,7 +76,7 @@ for record in recorddata:
 	r["learningresourcetype"] = "informatiebron"
 	r["interactivitytype"] = "expositive"
 	r["context"] = ["PO"]
-	r["typicalagerange"] = "8-12"
+	r["typicalagerange"] = "10-12"
 	r["copyrightandotherrestrictions"] = "cc-by-sa-30"
 	r["identifier"] = [ { "catalog": "URI", "value": host + record["url_title"] } ]
 	r["title"] = record["title"]
